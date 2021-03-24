ba5b362e122361c87f52baf4c12135c566d29898
Ajith madhan <ajithmadhan.dev113@gmail.com>
2021-03-24 22:20:48 +0530

    signout function

--- src/auth/helper/index.js
+++ src/auth/helper/index.js
@@ -60,6 +60,7 @@ export const isAuthenticated =()=>{
     }
     if(localStorage.getItem("jwt")){
         return JSON.parse(localStorage.getItem("jwt"));
+       
     }else{
         return false;
     }
